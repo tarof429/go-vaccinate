ca6701de1d58b23a79f442854c38c0a582e6a725
Taro Fukunaga <tarof429@gmail.com>
2020-08-13 18:17:09 -0700

    Get the simulation working for hardcoded values

--- Main.go
+++ Main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"text/tabwriter"
 	"time"
 
 	"github.com/c-bata/go-prompt"
@@ -18,35 +21,59 @@ import (
 type Person struct {
 	ID           int
 	SickDay      int
+	age          int
 	InfectedFlag bool
+	attributes   *PersonListAttributes
 }
 
 type PersonNode struct {
-	person Person
-	next   *PersonNode
+	person   Person
+	next     *PersonNode
+	previous *PersonNode
+}
+
+type PersonListAttributes struct {
+	maxSneeze         int
+	infectionRate     int
+	maxSickDays       int
+	numberOfPeople    int
+	infectedCount     int
+	sneezeProbability *rand.Rand
 }
 
 type PersonList struct {
-	maxSneeze     int
-	infectionRate int
-	maxSickDays   int
-	head          *PersonNode
-	tail          *PersonNode
+	attributes PersonListAttributes
+	head       *PersonNode
+	tail       *PersonNode
 }
 
-func NewPerson(id int, sickDay int, infectedFlag bool) Person {
-	return Person{id, sickDay, infectedFlag}
+func (list *PersonList) NewPerson(id int, sickDay int, age int, infectedFlag bool) Person {
+	return Person{id, sickDay, age, infectedFlag, &list.attributes}
 }
 
 func NewPersonNode(person Person) *PersonNode {
-	return &PersonNode{person, nil}
+	return &PersonNode{person, nil, nil}
 }
 
-func NewPersonList(maxSneeze, infectionRate, maxSickDays, count int) *PersonList {
-	list := PersonList{maxSneeze, infectionRate, maxSickDays, nil, nil}
+func NewPersonList(maxSneeze, infectionRate, maxSickDays, numberOfPeople, infectedCount int) *PersonList {
 
-	for i := 0; i < count; i++ {
-		p := NewPerson(i, 0, false)
+	attributes := PersonListAttributes{
+		maxSneeze,
+		infectionRate,
+		maxSickDays,
+		numberOfPeople,
+		infectedCount,
+		rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+
+	list := PersonList{
+		attributes,
+		nil,
+		nil,
+	}
+
+	for i := 0; i < numberOfPeople; i++ {
+		p := list.NewPerson(i, 0, 0, false)
 		list.Add(p)
 	}
 
@@ -59,11 +86,16 @@ func (list *PersonList) Add(p Person) {
 
 	if list.head == nil {
 		list.head = node
-		node.next = node
+		node.next = list.head
+		node.previous = list.tail
+		list.head.previous = node
 		list.tail = node
+
 	} else {
 		node.next = list.head
+		node.previous = list.tail
 		list.tail.next = node
+		list.head.previous = node
 		list.tail = node
 	}
 }
@@ -85,19 +117,115 @@ func (list *PersonList) List() {
 	}
 }
 
-func (list *PersonList) Visit() {
+func (list *PersonList) GatherStats() {
+
+	cur := list.head
+
+	headAddr := list.head
+
+	list.attributes.infectedCount = 0
+
+	for cur != nil {
+		if cur.person.InfectedFlag == true {
+			list.attributes.infectedCount++
+		}
+
+		//fmt.Println(cur.person)
+		cur = cur.next
+
+		if cur == headAddr {
+			break
+		}
+
+	}
+}
+
+func (list *PersonList) Print() {
+
+	// cur := list.head
+
+	// headAddr := list.head
+
+	// for cur != nil {
+	// 	fmt.Println(cur.person)
+	// 	cur = cur.next
+
+	// 	if cur == headAddr {
+	// 		break
+	// 	}
+
+	// }
+
+	w := tabwriter.NewWriter(os.Stdout, 2, 2, 4, ' ', 0)
+
+	defer w.Flush()
+
+	show := func(a, b interface{}) {
+		fmt.Fprintf(w, "%v\t%v\n", a, b)
+	}
+
+	show("COLUMN", "VALUE")
+	show("People", list.attributes.numberOfPeople)
+	show("Infected", list.attributes.infectedCount)
+
+}
+
+func (list *PersonList) ReverseList() {
+
+	cur := list.tail
+
+	headAddr := list.tail
+
+	for cur != nil {
+		fmt.Println(cur.person)
+		cur = cur.previous
+
+		if cur == headAddr {
+			break
+		}
+	}
+}
+
+func (list *PersonList) Visit(times int) {
+
+	cur := list.head
+
+	iteration := 0
+
+	for cur != nil {
+
+		if times != 0 && iteration > times {
+			break
+		}
+
+		list.attributes.infectedCount = 0
+
+		cur.Epoch()
+
+		if cur.person.InfectedFlag == true {
+			list.attributes.infectedCount++
+		}
+		cur = cur.next
+
+		if times != 0 {
+			iteration++
+		}
+	}
+}
+
+func (list *PersonList) ReverseVisit() {
 
 	cur := list.head
 
 	for cur != nil {
 		fmt.Println(cur.person)
-		cur = cur.next
+		cur = cur.previous
 		time.Sleep(100 * time.Millisecond)
 	}
 }
 
 func (p Person) String() string {
-	return fmt.Sprintf("ID: %d, Infected: %v, SickDay: %d, ", p.ID, p.InfectedFlag, p.SickDay)
+	return fmt.Sprintf("ID: %d, Infected: %v, SickDay: %d, Age: %d", p.ID, p.InfectedFlag, p.SickDay, p.age)
 }
 
 // func Init() {
@@ -123,15 +251,35 @@ func (p Person) String() string {
 // 	return sickFlag
 // }
 
-// func Epoch(persons []Person) {
+func (node *PersonNode) Epoch() {
 
-// 	for _, person := range persons {
-// 		person.Sneeze()
+	if node.person.InfectedFlag == true {
 
-// 		fmt.Println(person)
-// 	}
+		listAttributes := node.person.attributes
 
-// }
+		var probability int
+
+		// Previous
+		probability = listAttributes.sneezeProbability.Intn(100)
+
+		if probability <= listAttributes.infectionRate {
+			node.previous.person.InfectedFlag = true
+		}
+
+		// Next
+		probability = listAttributes.sneezeProbability.Intn(100)
+
+		if probability <= listAttributes.infectionRate {
+			node.next.person.InfectedFlag = true
+			node.person.SickDay++
+		}
+
+		if node.person.SickDay > node.person.attributes.maxSickDays {
+			node.person.InfectedFlag = false
+			node.person.SickDay = 0
+		}
+	}
+}
 
 func completer(d prompt.Document) []prompt.Suggest {
 	s := []prompt.Suggest{
@@ -142,47 +290,20 @@ func completer(d prompt.Document) []prompt.Suggest {
 	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
 }
 
-// func PersonList() []Person {
-
-// 	ids := r.Perm(MaxPeople)
-
-// 	persons := make([]Person, MaxPeople)
-
-// 	for i, id := range ids {
-// 		persons[i] = Person{ID: id}
-// 	}
-
-// 	return persons
-// }
-
 func Sleep() {
 	time.Sleep(time.Nanosecond * 1000)
 }
 
 func main() {
 
-	// Init()
+	persons := NewPersonList(3, 10, 3, 100, 0)
 
-	persons := NewPersonList(3, 3, 3, 10)
+	persons.head.person.InfectedFlag = true
 
+	persons.Visit(10000)
 	persons.List()
-
-	// Sleep()
-
-	// for _, person := range persons {
-	// 	person.Sneeze()
-	// 	fmt.Println(person)
-	// }
-
-	// for _, person := range persons {
-	// 	fmt.Println(person)
-	// }
-
-	// f := Facility{
-	// 	id:           "1234",
-	// 	maxOccupancy: 4,
-	// 	patients:     []Patient{p},
-	// }
+	persons.GatherStats()
+	persons.Print()
 
 	// for {
 	// 	fmt.Println("Please select command")
